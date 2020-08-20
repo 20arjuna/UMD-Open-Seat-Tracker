c38f5d9c3c5cafa156752cc512331f3b399c7d24
Arjun Akkiraju <20arjuna@students.harker.org>
2020-08-20 12:17:09 -0700

    done

--- driver.py
+++ driver.py
@@ -1,7 +1,22 @@
 import requests
+import re
 
 if __name__ == '__main__':
     dept = input("Enter code for the department offering the class (ex: MATH, CMSC): ")
     number = input("Enter the class number (ex: 216, 101): ")
     class_name = dept + number
-    print(class_name)
+
+    url = "http://app.testudo.umd.edu/soc/search?courseId=" + class_name + "&sectionId=&termId=202008&_openSectionsOnly=on&creditCompare=&credits=&courseLevelFilter=ALL&instructor=&_facetoface=on&_blended=on&_online=on&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&teachingCenter=ALL&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on"
+    r = requests.get(url)
+    code = r.text
+
+    indeces = [m.start() for m in re.finditer('open-seats-count">', code)]
+    htmlLen = len('open-seats-count">')
+    openSeats = []
+
+    for i in indeces:
+        openSeats.append(code[i + htmlLen : i + htmlLen + 2])
+
+    openSeats = [int(s.replace('<', '')) for s in openSeats]
+    print(sum(openSeats))
+
